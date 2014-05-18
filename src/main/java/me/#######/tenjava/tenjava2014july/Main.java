a31ae3473a50cadca5056c8f9a24b5dab0817852
Vilsol <vilsol2000@gmail.com>
2014-05-19 00:16:55 +0100

    Some template files

---
+++ src/main/java/me/#######/tenjava/tenjava2014july/Main.java
@@ -0,0 +1,15 @@
+package me.#####.tenjava.tenjava2014july;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable(){
+		Logger l = Bukkit.getLogger();
+		l.info("TenJava 2014 July Entry by Vilsol for theme ######!");
+	}
+	
+}
