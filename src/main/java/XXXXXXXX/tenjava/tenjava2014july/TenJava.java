8d19708a5ef412c320a9d86633601ef2baaf589e
Vilsol <vilsol2000@gmail.com>
2014-05-19 12:35:57 +0100

    A few additions to .gitignore, Changing Main.java to TenJava.java

--- src/main/java/XXXXXXXX/tenjava/tenjava2014july/TenJava.java
+++ src/main/java/XXXXXXXX/tenjava/tenjava2014july/TenJava.java
@@ -2,7 +2,7 @@ package XXXXXXXX.tenjava.tenjava2014july;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class TenJava extends JavaPlugin {
 	
 	public void onEnable(){
 		getLogger().info("TenJava 2014 July Entry by XXXXXXXX for theme XXXXXXXX!");
